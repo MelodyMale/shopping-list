25209d513f6414a93142a17dd1802f2c3779d14f
chayutpong <chayutpong@creditok.co>
2023-01-08 18:12:08 +1100

    feat: add item model serializers

--- core/serializers.py
+++ core/serializers.py
@@ -2,16 +2,7 @@ from rest_framework import serializers
 from core.models import Item
 
 
-class ItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=False, max_length=100)
-    checked = serializers.BooleanField(required=False)
-
-    def create(self, validated_data):
-        return Item.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.checked = validated_data.get("checked", instance.checked)
-        instance.save()
-        return instance
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ["id", "name", "checked"]
